b4b44f7583f0cbfe904bc338652f76656cfc9d0b
weliton <welingtonzsilva@gmail.com>
2023-10-26 10:53:36 -0300

    phpunit configurado e add teste para abstract entity

---
+++ src/Domain/Entities/AbstractEntity.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Entities;
+
+use DateTimeImmutable;
+use JsonSerializable;
+use UnexpectedValueException;
+
+use function json_decode;
+use function json_encode;
+
+use const DATE_ATOM;
+
+abstract class AbstractEntity implements JsonSerializable
+{
+    protected int $id;
+
+    protected int $createdAt;
+
+    protected ?int $deletedAt;
+
+    protected ?int $updatedAt;
+
+    public function __construct(int $id, int $createdAt, ?int $deletedAt, ?int $updatedAt)
+    {
+        $this->id           = $id;
+        $this->createdAt    = $createdAt;
+        $this->deletedAt    = $deletedAt;
+        $this->updatedAt    = $updatedAt;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedAt(): string
+    {
+        return (new DateTimeImmutable())
+            ->setTimestamp($this->createdAt)
+            ->format(DATE_ATOM);
+    }
+
+    public function getCreatedAtFromTimeStanp(): int
+    {
+        return $this->createdAt;
+    }
+
+    public function getDeletedAt(): ?string
+    {
+        return (new DateTimeImmutable())
+            ->setTimestamp($this->deletedAt)
+            ->format(DATE_ATOM);
+    }
+
+    public function getDeletedAtFromTimeStanp(): ?int
+    {
+        return $this->deletedAt;
+    }
+
+    public function getUpdatedAt(): ?string
+    {
+        return (new DateTimeImmutable())
+            ->setTimestamp($this->updatedAt)
+            ->format(DATE_ATOM);
+    }
+
+    public function getUpdatedAtFromTimeStanp(): ?int
+    {
+        return $this->deletedAt;
+    }
+
+    public function toArray(): array
+    {
+        $json = json_encode(get_object_vars($this));
+
+        if ($json === false) {
+            throw new UnexpectedValueException('Could not decode atributes to json');
+        }
+
+        return json_decode($json, true);
+    }
+
+    public function jsonSerialize(): mixed
+    {
+        return $this->toArray();
+    }
+}
---
+++ tests/Unit/Domain/Entities/AbstractEntityTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\Unit\Domain\Entities;
+
+use DateTimeImmutable;
+use Domain\Entities\AbstractEntity;
+use Tests\TestCase;
+
+use const DATE_ATOM;
+/**
+ * @covers Domain\Entities\AbstractEntity
+ */
+final class AbstractEntityTest extends TestCase
+{
+    public function testCreateInstance(): void
+    {
+        $id                     = 1;
+        $createdAt              = (new DateTimeImmutable())->format(DATE_ATOM);
+        $createFromTimeStamp    = (new DateTimeImmutable())->getTimestamp();
+        $toArray                = ['id' => $id, 'createdAt' => $createFromTimeStamp, 'deletedAt' => null, 'updatedAt' => null];
+
+        $entity = new class($id, $createFromTimeStamp, null, null) extends AbstractEntity
+        {
+            protected int $id;
+            protected int $createdAt;
+            protected ?int $deletedAt;
+            protected ?int $updatedAt;
+
+            public function __construct(int $id, int $createdAt, ?int $deletedAt, ?int $updatedAt)
+            {
+                $this->id           = $id;
+                $this->createdAt    = $createdAt;
+                $this->deletedAt    = $deletedAt;
+                $this->updatedAt    = $updatedAt;
+            }
+        };
+
+        $this->assertSame($id, $entity->getId());
+        $this->assertSame($createdAt, $entity->getCreatedAt());
+        $this->assertSame($toArray, $entity->toArray());
+    }
+}
