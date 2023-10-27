1d0d0989b56fed6cefb584e379b401c2cb470ff9
weliton <welingtonzsilva@gmail.com>
2023-10-27 15:01:33 -0300

    criado value object Email e implementado testes de unidade

---
+++ src/Domain/ValueObjects/Email.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\ValueObjects;
+
+use InvalidArgumentException;
+use JsonSerializable;
+use Stringable;
+
+final class Email implements Stringable, JsonSerializable
+{
+    private string $email;
+
+    public function __construct(string $email)
+    {
+       if ($this->isValid($email)) {
+           $this->email = $email;
+        }
+    }
+
+    public function value(): string
+    {
+        return $this->email;
+    }
+
+    public static function create(string $value): self
+    {
+        return new self($value);
+    }
+
+    public function equals(Email $email): bool
+    {
+        return $this->email === $email->value();
+    }
+
+    public function jsonSerialize(): mixed
+    {
+        return $this->value();
+    }
+
+    public function __toString(): string
+    {
+        return $this->value();
+    }
+
+    private function isValid(string $value): bool
+    {
+        $patters    = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/';
+        $validate   = preg_match($patters, $value);
+
+        if (!$validate) {
+            throw new InvalidArgumentException('Email is not valid', 400);
+        }
+
+        return true;
+    }
+}
---
+++ tests/Unit/Domain/ValueObjects/EmailTest.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\Unit\Domain\ValueObjects;
+
+use Domain\ValueObjects\Email;
+use InvalidArgumentException;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Tests\TestCase;
+
+/**
+ * @covers Domain\Entities\ValueObjects\Email
+ */
+final class EmailTest extends TestCase
+{
+
+    public function testInstantiateEmailFromStaticMethod(): void
+    {
+        $expected   = 'welingtonzsilva@gmail.com';
+        $email      = Email::create($expected);
+
+        $this->assertSame($expected, $email->value());
+    }
+
+    #[DataProvider('invalidEmail')]
+    public function testThrowExceptionIfEmailIsNotValid(string $value): void
+    {
+        $this->expectExceptionMessage('Email is not valid');
+        $this->expectExceptionCode(400);
+        $this->expectException(InvalidArgumentException::class);
+        Email::create($value);
+    }
+
+    public static function invalidEmail(): array
+    {
+        return [
+            ['welingtonzsilvagmail.com'],
+            ['welingtonzsilva@gmail'],
+            ['welington z silva@gmail.com.br'],
+            ['welington/silva@gmail.com.br'],
+        ];
+    }
+
+    public function testEqualsWhenTheObjectWereTheSame(): void
+    {
+        $email      = 'welingtonzsilva@gmail.com';
+        $emailOne   = Email::create($email);
+        $emailTwo   = Email::create($email);
+
+        $this->assertTrue($emailOne->equals($emailTwo));
+    }
+
+    public function testEqualsWhenTheObjectWereDiferent(): void
+    {
+        $email      = 'welingtonzsilva@gmail.com';
+        $emailOne   = Email::create($email);
+        $emailTwo   = Email::create('jvalentine@umbrella.com');
+
+        $this->assertFalse($emailOne->equals($emailTwo));
+    }
+}
