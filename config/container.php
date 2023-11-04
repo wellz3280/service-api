3106a11b62ecaeef95d6573ee6c6468714a1bce0
weliton <welingtonzsilva@gmail.com>
2023-11-04 02:18:18 -0300

    add Interface de banco de dados para testes

--- config/container.php
+++ config/container.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace Config;
 
+use Domain\Interfaces\MysqlDbTestInterface;
+use Infra\Factories\MysqlDbTestFactory;
 use Infra\Factories\MysqlFactory;
 use PDO;
 
 use function DI\factory;
 
 return [
-    PDO::class => factory(MysqlFactory::class),
+    MysqlDbTestInterface::class => factory(MysqlDbTestFactory::class),
+    PDO::class              => factory(MysqlFactory::class),
 ];
