b50fe912d5db49ff6c8bdd11bd2668c2469c4d73
weliton <welingtonzsilva@gmail.com>
2023-11-25 22:52:17 -0300

    feat: add rota para controller getUser

--- config/routes.php
+++ config/routes.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 use Infra\Http\Controller\PingController;
 use Infra\Http\Controller\CreateUserController;
+use Infra\Http\Controller\GetUserController;
 use Slim\App;
 
 return function (App $app) {
     $app->get('/ping', PingController::class);
     $app->post('/users', CreateUserController::class);
+    $app->get('/users', GetUserController::class);
 };
