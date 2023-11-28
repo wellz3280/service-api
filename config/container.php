bca82d01ce0b7502a1881ac7824e6325e421c160
weliton <welingtonzsilva@gmail.com>
2023-11-28 15:25:16 -0300

    feat: add App ao container de dependência

--- config/container.php
+++ config/container.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace Config;
 
 use Domain\Interfaces\UserRepositoryInterface;
+use Infra\Factories\AppFactory;
 use Infra\Factories\MysqlFactory;
 use Infra\Persistence\UserRepository;
 use PDO;
+use Slim\App;
 
 use function DI\autowire;
 use function DI\factory;
 
 return [
+    App::class                      => factory(AppFactory::class),
     PDO::class                      => factory(MysqlFactory::class),
     UserRepositoryInterface::class  => autowire(UserRepository::class),
 ];
