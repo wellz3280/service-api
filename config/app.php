47d22f3957807c46b761dd78b68c237a71955387
weliton <welingtonzsilva@gmail.com>
2023-11-28 15:24:07 -0300

    feat: add nodos para config de app.php

--- config/app.php
+++ config/app.php
@@ -7,4 +7,7 @@ use function DI\env;
 return [
     'name' => env('APP_NAME', 'service-api'),
     'env'   => env('APP_ENVIROMENT', 'local'),
+    'debug' => '',
+    'baseUrl' => env('APP_BASE_URL', 'v1'),
+    'url'     => env('APP_URL', 'http://localhost:8081'),
 ];
