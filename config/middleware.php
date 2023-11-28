260b81bd515b6bbc2590d9b1aa7c48250991edf5
weliton <welingtonzsilva@gmail.com>
2023-11-28 15:26:20 -0300

    chore: retirado trecho de código inútil

--- config/middleware.php
+++ config/middleware.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 use Slim\App;
 
-require __DIR__ . '/../vendor/autoload.php';
-
 return function (App $app) {
     $app->addBodyParsingMiddleware();
     $app->addRoutingMiddleware();
