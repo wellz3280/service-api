65039b423d955c00ddf6ba989bbcad1de05c8aea
weliton <welingtonzsilva@gmail.com>
2023-11-28 15:39:38 -0300

    feat: criado inputModel e add validação para id

---
+++ src/Application/GetUserById/InputModel.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\GetUserById;
+
+use Application\AbstractInputModel;
+use Domain\Exceptions\ValidationException;
+use Respect\Validation\Exceptions\NestedValidationException;
+use Respect\Validation\Validator as v;
+
+final class InputModel extends AbstractInputModel
+{
+    public function getId(): int
+    {
+        return (int) $this->getPayload()['id'];
+    }
+
+    /**
+     * @param mixed[] $data
+     */
+    public function throwExceptionOnFailure(array $data): void
+    {
+        try {
+            v::key('id', v::numericVal()->setName('id'))
+            ->assert($data['payload']);
+        } catch (NestedValidationException $exception) {
+            throw ValidationException::create($exception->getMessages([
+                'id' => 'id must be type integer',
+            ]));
+        }
+    }
+}
