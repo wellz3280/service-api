df86f291a80b1f7b1ae69b42d5ce2442846ea526
weliton <welingtonzsilva@gmail.com>
2023-11-28 15:24:42 -0300

    refactor: add container builder

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,6 +2,13 @@
 
 declare(strict_types=1);
 
-use Infra\Factories\ContainerFactory;
+use DI\ContainerBuilder;
 
-return ContainerFactory::create();
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->addDefinitions(__DIR__ . '/container.php');
+$containerBuilder->addDefinitions([
+    'app'       => require __DIR__ . '/app.php',
+    'services'  => require __DIR__ . '/services.php',
+]);
+
+return $containerBuilder->build();
