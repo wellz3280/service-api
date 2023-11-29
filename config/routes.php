07bce5f9b9bacfac4f9b62538fe7731db6c8ec41
weliton <welingtonzsilva@gmail.com>
2023-11-29 18:29:46 -0300

    feat: add rota para updateUserController

--- config/routes.php
+++ config/routes.php
@@ -6,6 +6,7 @@ use Infra\Http\Controller\CreateUserController;
 use Infra\Http\Controller\GetUserByIdController;
 use Infra\Http\Controller\GetUserController;
 use Infra\Http\Controller\PingController;
+use Infra\Http\Controller\UpdateUserController;
 use Slim\App;
 use Slim\Routing\RouteCollectorProxy;
 
@@ -14,6 +15,7 @@ return function (App $app, string $baseUrl) {
 
     $app->group('', function (RouteCollectorProxy $group) {
         $group->get('/ping', PingController::class);
+        $group->put('/users/{id}', UpdateUserController::class);
         $group->get('/users/{id}', GetUserByIdController::class);
         $group->post('/users', CreateUserController::class);
         $group->get('/users', GetUserController::class);
