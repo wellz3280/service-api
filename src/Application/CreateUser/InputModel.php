b87771d04f4837560b31be26930206d29e06797d
weliton <welingtonzsilva@gmail.com>
2023-11-28 23:55:09 -0300

    feat: add metodo para obter password

--- src/Application/CreateUser/InputModel.php
+++ src/Application/CreateUser/InputModel.php
@@ -5,12 +5,20 @@ declare(strict_types=1);
 namespace Application\CreateUser;
 
 use Application\AbstractInputModel;
+use Domain\Exceptions\ValidationException;
 use Domain\ValueObjects\Email;
+use Domain\ValueObjects\Password;
 use Respect\Validation\Exceptions\NestedValidationException;
+use Respect\Validation\Exceptions\ValidatorException;
 use Respect\Validation\Validator as v;
 
 final class InputModel extends AbstractInputModel
 {
+    public function getPassword(): Password
+    {
+        return Password::create($this->getPayload()['password']);
+    }
+
     public function getName(): string
     {
         return $this->payload['name'];
@@ -28,9 +36,12 @@ final class InputModel extends AbstractInputModel
     {
         try {
             v::key('name', v::stringType()->notEmpty()->setName('name'))
+                ->key('password', v::stringType()->notEmpty())
             ->assert($data['payload']);
         } catch (NestedValidationException $exception) {
-            echo $exception->getFullMessage();
+            throw ValidationException::create($exception->getMessages([
+                'password' => 'password must be present.',
+            ]));
         }
     }
 }
