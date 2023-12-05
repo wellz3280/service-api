e8e631d46fbbe32d01c43a69ca0107feef459c72
weliton <welingtonzsilva@gmail.com>
2023-12-04 23:35:25 -0300

    feat: criado dto InputModel e add validação de dados

---
+++ src/Application/DeleteUser/InputModel.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\DeleteUser;
+
+use Application\AbstractInputModel;
+use Domain\Exceptions\ValidationException;
+use Respect\Validation\Exceptions\NestedValidationException;
+use Respect\Validation\Validator as v;
+
+final class InputModel extends AbstractInputModel
+{
+    public function getId(): int
+    {
+        return (int) $this->getPayload()['id'];
+    }
+
+    public function throwExceptionOnFailure(array $data): void
+    {
+        try {
+            v::key('id', v::numericVal()->setName('id'))
+            ->assert($data['payload']);
+        } catch (NestedValidationException $exception) {
+            throw ValidationException::create($exception->getMessages([
+                'id' => 'id must be type integer',
+            ]));
+        }
+    }
+}
